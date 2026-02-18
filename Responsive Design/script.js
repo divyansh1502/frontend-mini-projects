c8dabaf306e3a0380644cf4e11e148f31aa2ac9e
Divyansh Singh <157667678+divyansh1502@users.noreply.github.com>
2026-02-18 13:00:32 +0530

    Food app ui

---
+++ Responsive Design/script.js
@@ -0,0 +1,9 @@
+let a = Number(prompt("Give a number"));
+for(let i = 1; i <= a; i++) {
+    if(i % 2 == 0) {
+        console.log(`${i} is even`);
+    }
+    else {
+        console.log(`${i} is odd`);
+    }
+} 
